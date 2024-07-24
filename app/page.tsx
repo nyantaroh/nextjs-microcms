17ca2db80d920bcd61e5ec323d2886252bdf6963
nyantaroh <spicalgenib09@gmail.com>
2024-07-24 20:48:27 +0900

    Chapter3まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-type News = {
-  id: string;
-  title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { News } from "@/app/_libs/microcms";
 
 const data: { contents: News[] } = {
   contents: [
@@ -57,25 +51,10 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <ul>
-          {sliceData.map((article) => (
-            <li key={article.id} className={styles.list}>
-              <div className={styles.link}>
-                <Image src="/no-image.png" width={1200} height={630} className={styles.image} alt="No Image" />
-                <dl className={styles.content}>
-                  <dt className={styles.newsItemTitle}>{article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.tag}>{article.category.name}</span>
-                    <span className={styles.date}>
-                      <Image src="/clock.svg" width={16} height={16} priority alt="" />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
       </section>
     </>
   );
