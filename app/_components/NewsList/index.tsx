ba92279f9d926047f422d9086977cfa67dad9910
nyantaroh <spicalgenib09@gmail.com>
2024-07-29 22:02:41 +0900

    Chapter4まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.css";
 import Category from "../Category";
@@ -18,7 +19,7 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          <div className={styles.link}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
             <Image src="/no-image.png" width={1200} height={630} className={styles.image} alt="No Image" />
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
@@ -27,7 +28,7 @@ export default function NewsList({ news }: Props) {
                 <Date date={article.publishedAt ?? article.createdAt} />
               </dd>
             </dl>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
